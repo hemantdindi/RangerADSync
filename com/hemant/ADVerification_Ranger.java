b79fdad100e354f87dbe457b4269c5eda2de9adf
hemant <hemantkumar.dindi@gmail.com>
2016-11-05 02:23:58 +0530

    AD Verification and Validation

---
+++ com/hemant/ADVerification_Ranger.java
@@ -0,0 +1,112 @@
+package com.hemant.ranger;
+import java.util.Hashtable;
+import javax.naming.Context;
+import javax.naming.NamingEnumeration;
+import javax.naming.directory.Attributes;
+import javax.naming.directory.SearchControls;
+import javax.naming.directory.SearchResult;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
+
+class ADVerification_Ranger
+{
+	DirContext ldapContext;
+	
+	public void createADConn(){
+		connect2AD();
+	}
+	public void closeADConn()
+	{
+		try{
+			ldapContext.close();
+		}catch(Exception e){}
+	}
+	public void connect2AD(){
+		try{
+		      Hashtable<String, String> ldapEnv = new Hashtable<String, String>(11);
+		      ldapEnv.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+		      ldapEnv.put(Context.PROVIDER_URL,  "");
+		      ldapEnv.put(Context.SECURITY_AUTHENTICATION, "simple");
+		      ldapEnv.put(Context.SECURITY_PRINCIPAL, "");
+		      ldapEnv.put(Context.SECURITY_CREDENTIALS, "");
+		      //ldapEnv.put(Context.SECURITY_PROTOCOL, "ssl");
+		      ldapEnv.put(Context.SECURITY_PROTOCOL, "simple");
+		      ldapContext = new InitialDirContext(ldapEnv);
+			
+		}catch (Exception e)
+	    {
+		      System.out.println(" Search error: " + e);
+		      e.printStackTrace();
+		      System.exit(-1);
+		    }
+		
+	}
+  
+  public boolean SearchUser(String accountName) throws Exception
+  {
+	  // Create the search controls         
+      SearchControls searchCtls = new SearchControls();
+      //Specify the attributes to return
+      String returnedAtts[]={"sn","givenName", "samAccountName"};
+      searchCtls.setReturningAttributes(returnedAtts);
+      //Specify the search scope
+      searchCtls.setSearchScope(SearchControls.SUBTREE_SCOPE);
+      //specify the LDAP search filter
+      //String searchFilter = "(&(objectClass=group)(sAMAccountName=" + accountName + "))";      
+      String searchFilter = "(&(objectClass=user)(sAMAccountName=" + accountName + "))";
+      //Specify the Base for the search
+      String searchBase = "ou=HDP,ou=EEDC,dc=emea,dc=corpdir,dc=net";
+      //initialize counter to total the results
+      int totalResults = 0;
+      // Search for objects using the filter
+      NamingEnumeration<SearchResult> answer = ldapContext.search(searchBase, searchFilter, searchCtls);
+
+      //Loop through the search results
+      while (answer.hasMoreElements())
+      {
+        SearchResult sr = (SearchResult)answer.next();
+        totalResults++;
+        @SuppressWarnings("unused")
+		Attributes attrs = sr.getAttributes();
+        //System.out.println("Username : " + attrs.get("samAccountName"));
+      }
+      if (totalResults == 1)
+    	  return true;
+      else
+    	  return false;
+  }
+  
+  public boolean SearchGroup(String accountName) throws Exception
+  {
+	  // Create the search controls         
+      SearchControls searchCtls = new SearchControls();
+      //Specify the attributes to return
+      String returnedAtts[]={"sn","givenName", "samAccountName"};
+      searchCtls.setReturningAttributes(returnedAtts);
+      //Specify the search scope
+      searchCtls.setSearchScope(SearchControls.SUBTREE_SCOPE);
+      //specify the LDAP search filter
+      String searchFilter = "(&(objectClass=group)(sAMAccountName=" + accountName + "))";      
+      //String searchFilter = "(&(objectClass=user)(sAMAccountName=" + accountName + "))";
+      //Specify the Base for the search
+      String searchBase = "";
+      //initialize counter to total the results      
+      int totalResults = 0;
+      // Search for objects using the filter
+      NamingEnumeration<SearchResult> answer = ldapContext.search(searchBase, searchFilter, searchCtls);
+
+      //Loop through the search results
+      while (answer.hasMoreElements())
+      {
+        SearchResult sr = (SearchResult)answer.next();
+        totalResults++;
+        @SuppressWarnings("unused")
+		Attributes attrs = sr.getAttributes();
+        //System.out.println("Group    : "  + attrs.get("samAccountName"));
+      }
+      if (totalResults == 1)
+    	  return true;
+      else
+    	  return false;
+  }
+}
