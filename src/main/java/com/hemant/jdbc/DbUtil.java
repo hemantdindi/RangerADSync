ba50cb87a520fc52bf8ec541a873b5c02777c237
hemant <hemant@DESKTOP-UICB1KI>
2016-11-08 19:47:39 +0530

    updated DBUtil

--- src/main/java/com/hemant/jdbc/DbUtil.java
+++ src/main/java/com/hemant/jdbc/DbUtil.java
@@ -14,6 +14,7 @@ import java.time.ZoneOffset;
 public class DbUtil {
 	private Connection connection = null;
 	
+	
     public Connection getConnection(ReadProperties conf) {
             try {
                 
